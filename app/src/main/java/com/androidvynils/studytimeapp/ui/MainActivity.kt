ccbdeb8a6d5765e102c15be8c67e67cdb47cebe5
Miguel Tovar <miguel@miguel.com>
2024-03-09 18:02:01 -0500

    finishing signin

--- app/src/main/java/com/androidvynils/studytimeapp/ui/MainActivity.kt
+++ app/src/main/java/com/androidvynils/studytimeapp/ui/MainActivity.kt
@@ -3,7 +3,10 @@ package com.studytime.app.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
 import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 import com.studytime.app.R
 
 class MainActivity : AppCompatActivity() {
@@ -11,11 +14,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //val button = findViewById<Button>(R.id.button_id)
-        /*button.setOnClickListener {
+        val textInputLayoutPassword = findViewById<TextInputLayout>(R.id.pwdText)
+        val editTextPassword = findViewById<TextInputEditText>(R.id.editTextPassword)
+
+        textInputLayoutPassword.setEndIconOnClickListener {
+            val currentInputType = editTextPassword.inputType
+
+            if (currentInputType == InputType.TYPE_TEXT_VARIATION_PASSWORD or InputType.TYPE_CLASS_TEXT) {
+                editTextPassword.inputType = InputType.TYPE_CLASS_TEXT
+            } else {
+                editTextPassword.inputType = InputType.TYPE_TEXT_VARIATION_PASSWORD or InputType.TYPE_CLASS_TEXT
+            }
+
+            editTextPassword.text?.let { it1 -> editTextPassword.setSelection(it1.length) }
+        }
+
+        val siginButton = findViewById<Button>(R.id.signInButton)
+        siginButton.setOnClickListener {
             val intent = Intent(this, DashboardActivity::class.java)
             startActivity(intent)
-        }*/
+        }
 
     }
 }
