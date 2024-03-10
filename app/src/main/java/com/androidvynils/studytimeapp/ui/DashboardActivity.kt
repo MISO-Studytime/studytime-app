2cb2604e11760689b472a5904aecc79a8f8d8cf6
zearkiatos <caprilespe@outlook.com>
2024-03-10 01:26:15 -0300

    Adding dialog

--- app/src/main/java/com/androidvynils/studytimeapp/ui/DashboardActivity.kt
+++ app/src/main/java/com/androidvynils/studytimeapp/ui/DashboardActivity.kt
@@ -1,6 +1,9 @@
 package com.studytime.app.ui
 
+import android.app.Dialog
 import android.graphics.Color
+import android.graphics.drawable.Drawable
+import android.graphics.drawable.GradientDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -13,6 +16,8 @@ import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.bottomappbar.BottomAppBar
 import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.internal.ScrimInsetsFrameLayout
 import com.google.android.material.math.MathUtils
 import com.google.android.material.navigation.NavigationView
@@ -36,5 +41,71 @@ class DashboardActivity : AppCompatActivity() {
             drawerLayout.close()
             true
         }
+
+        val cancelButton1 = this.findViewById<MaterialButton>(R.id.cancel_button_1)
+        cancelButton1.setOnClickListener{
+            val dialog = Dialog(this)
+            dialog.setContentView(R.layout.custom_dialog_layout)
+
+            dialog.show()
+
+
+            val yes = dialog.findViewById<MaterialButton>(R.id.yes_button)
+            val no = dialog.findViewById<MaterialButton>(R.id.no_button)
+
+            yes.setOnClickListener {
+                dialog.dismiss()
+            }
+
+            no.setOnClickListener {
+                dialog.dismiss()
+            }
+        }
+
+    val cancelButton2 = this.findViewById<MaterialButton>(R.id.cancel_button_2)
+        cancelButton2.setOnClickListener{
+            val dialog = Dialog(this)
+            dialog.setContentView(R.layout.custom_dialog_layout)
+
+            dialog.show()
+
+
+            val yes = dialog.findViewById<MaterialButton>(R.id.yes_button)
+            val no = dialog.findViewById<MaterialButton>(R.id.no_button)
+
+            yes.setOnClickListener {
+                dialog.dismiss()
+            }
+
+            no.setOnClickListener {
+                dialog.dismiss()
+            }
+        }
+
+       val cancelButton3 = this.findViewById<MaterialButton>(R.id.cancel_button_3)
+       cancelButton3.setOnClickListener{
+           val dialog = Dialog(this)
+           dialog.setContentView(R.layout.custom_dialog_layout)
+
+           dialog.show()
+
+           val yes = dialog.findViewById<MaterialButton>(R.id.yes_button)
+           val no = dialog.findViewById<MaterialButton>(R.id.no_button)
+
+           yes.setOnClickListener {
+               dialog.dismiss()
+           }
+
+           no.setOnClickListener {
+               dialog.dismiss()
+           }
+
+
+
+
+       }
     }
+
+
+
 }
