363c19d25a811a2225c6e1f3b2ceee09035bf4b7
zearkiatos <caprilespe@outlook.com>
2024-03-12 14:42:15 -0300

    [WIP] Adding time picker component

--- app/src/main/java/com/androidvynils/studytimeapp/ui/CreateAlarmActivity.kt
+++ app/src/main/java/com/androidvynils/studytimeapp/ui/CreateAlarmActivity.kt
@@ -1,13 +1,18 @@
  package com.androidvynils.studytimeapp.ui
 
+import android.app.TimePickerDialog
 import android.icu.text.SimpleDateFormat
+import android.icu.util.Calendar
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import com.google.android.material.datepicker.MaterialDatePicker
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
 import com.studytime.app.R
+import java.sql.Time
 import java.util.Date
 import java.util.Locale
 
@@ -27,7 +32,33 @@ import java.util.Locale
         datePickerEndText.setOnClickListener {
             this.showDatePickerDialog(datePickerEndText)
         }
+
+        val startTime = findViewById<TextInputEditText>(R.id.select_dialog_time)
+
+        startTime.setOnClickListener{
+           this.showTimePickerDialog(startTime)
+        }
+
+        val endTime = findViewById<TextInputEditText>(R.id.select_dialog_time_end)
+
+        endTime.setOnClickListener{
+            this.showTimePickerDialog(endTime)
+        }
     }
+
+     private fun showTimePickerDialog(editText: TextInputEditText) {
+         val calendar = Calendar.getInstance()
+         val hour = calendar.get(Calendar.HOUR_OF_DAY)
+         val minute = calendar.get(Calendar.MINUTE)
+
+         val timePickerDialog = TimePickerDialog(this, TimePickerDialog.OnTimeSetListener { _, hourOfDay, minute ->
+             // Format the selected time
+             val formattedTime = formatTime(hourOfDay, minute)
+             // Set the formatted time to the text field
+             editText.setText(formattedTime)
+         }, hour, minute, false)
+         timePickerDialog.show()
+     }
 
     private fun showDatePickerDialog(editText: TextInputEditText) {
         val builder = MaterialDatePicker.Builder.datePicker()
@@ -48,4 +79,12 @@ import java.util.Locale
         // Show the date picker dialog
         picker.show(supportFragmentManager, picker.toString())
     }
+
+     private fun formatTime(hour: Int, minute: Int): String {
+         val calendar = Calendar.getInstance()
+         calendar.set(Calendar.HOUR_OF_DAY, hour)
+         calendar.set(Calendar.MINUTE, minute)
+         val dateFormat = SimpleDateFormat("hh:mm a", Locale.getDefault())
+         return dateFormat.format(calendar.time)
+     }
 }
